3edc4e225a2131a44993600a19e953a978ee1bf7
SteveoBurns <steveo.burns.777@gmail.com>
2021-04-23 19:54:25 +1000

    Finished off level 1 layout

--- Assets/Secret Garden/Scripts/PlayerController.cs
+++ Assets/Secret Garden/Scripts/PlayerController.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// This will handle all player pick ups
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
