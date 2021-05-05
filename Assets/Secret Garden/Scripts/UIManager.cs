fdeda32994bf44e4c198786079bcd13db5b219c5
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2021-05-05 23:38:46 +1000

    More Ui stuff
    more features and fixes

--- Assets/Secret Garden/Scripts/UIManager.cs
+++ Assets/Secret Garden/Scripts/UIManager.cs
@@ -9,12 +9,18 @@ public class UIManager : MonoBehaviour
 {
     public GameObject PauseMenu;
     public GameObject OptionsMenu;
+    public GameObject PetalPanel;
+    public GameObject TimerPanel;
+    public Text MasterVolumePercent;
+    public Text MusicVolumePercent;
 
     public AudioMixer GameMusic;
     public float volumeLevel;
     public float setVolumeLevel;
     public Slider volumeSlider;
     float mutedVolume = 0;
+    float masterVolume;
+    float musicVolume;
     public bool muted;
 
     public Text timerUI;
@@ -36,6 +42,14 @@ public class UIManager : MonoBehaviour
         nextScene = 0;
 
         muted = false;
+        masterVolume = 1;
+        musicVolume = 1;
+
+        if (nextScene == 0)
+        {
+            PetalPanel.SetActive(false);
+            TimerPanel.SetActive(false);
+        }
     }
 
     private void Update()
@@ -45,10 +59,18 @@ public class UIManager : MonoBehaviour
             PauseButton();
         }
 
-        timer = (int)(timerStart - Time.time);
 
         int minutes = Mathf.FloorToInt(timer / 60F);
         int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+        if (nextScene <= 3)
+        {
+            timer = (int)(timerStart - Time.time);
+        }
+        else
+        {
+            timer = (int)timerStart;
+        }
 
         string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
         timerUI.text = "Time Remaining: " + niceTime;
@@ -78,14 +100,44 @@ public class UIManager : MonoBehaviour
 
     public void ExitButton()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 
     public void Continue()
     {
+
         TimerTotal();
         nextScene++;
         SceneManager.LoadScene(nextScene);
+
+        if (nextScene >= 1)
+        {
+            PetalPanel.SetActive(true);
+            TimerPanel.SetActive(true);
+        }
+
+        switch (nextScene)
+        {
+            case 1:
+                timerStart = 300;
+                break;
+
+            case 2:
+                timerStart = 360;
+                break;
+
+            case 3:
+                timerStart = 420;
+                break;
+            case 4:
+                timerStart = timerTotal;
+                break;
+        }
+
     }
 
     public void RestartLevel()
@@ -96,7 +148,8 @@ public class UIManager : MonoBehaviour
 
     public void Music()
     {
-
+        MasterVolumePercent.text = (masterVolume / 1f).ToString() + "%";
+        MusicVolumePercent.text  = (musicVolume / 1f).ToString() + "%";
     }
 
     public void Mute()
