7d451b00f12cda3275840539de752bf60fda7d5b
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2021-06-04 12:03:43 +1000

    continuing edits

--- Assets/Secret Garden/Scripts/UIManager.cs
+++ Assets/Secret Garden/Scripts/UIManager.cs
@@ -324,7 +324,6 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Single);
 
         //doesnt destroy the UI elements needed in the game
-        if (nextScene > 0)
         DontDestroyOnLoad(UIManagerObject);
     }
 
